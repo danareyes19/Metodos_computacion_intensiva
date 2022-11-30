c749f99b3e772f0b40f85fee74281065513d75b8
danareyes19 <101271406+danareyes19@users.noreply.github.com>
2022-11-30 08:30:09 -0500

    Add files via upload

--- Proyecto/Remy/actions/actions.py
+++ Proyecto/Remy/actions/actions.py
@@ -9,12 +9,24 @@
 import pandas as pd 
 from typing import Any, Text, Dict, List
 #
+from sentence_transformers import SentenceTransformer
+from transformers import pipeline
+from sklearn.metrics.pairwise import cosine_similarity
+#
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
 #
 #
 data = pd.read_csv('https://raw.githubusercontent.com/danareyes19/Metodos_computacion_intensiva/main/Proyecto/Recetas.csv')
 RECETAS = list(data['Nombre receta'])
+INGREDIENTES = list(data['Ingredientes'])
+PREPARACION = lsit(data['Preparación'])
+
+model_name = 'hiiamsid/sentence_similarity_spanish_es'
+model = SentenceTransformer(model_name_or_path=model_name, device='cpu')
+embedding_bert_train_ing = model.encode(INGREDIENTES, show_progress_bar=True)
+embedding_bert_train_rec = model.encode(RECETAS, show_progress_bar=True)
+embedding_bert_train_prep = model.encode(PREPARACION, show_progress_bar=True)
 
 class ActionSearchRecipe(Action):
 
@@ -42,3 +54,48 @@ class ActionSearchRecipe(Action):
 
         return []
 
+class ActionSearchIngredients():
+    def name(self) -> Text:
+       return "action_search_ingredients"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) ->  List[Dict[Text, Any]]:
+            ingrediente = next(tracker.get_latest_entity_values("ingrediente"), None):
+
+            embedding_bert_ing = model.encode(ingrediente, show_progress_bar=False)
+            similarity_i = cosine_similarity(
+            [embedding_bert_ing],
+            embedding_bert_train_ing[0:]
+        )
+            similaridad_i = pd.DataFrame(similarity_i).transpose()
+            similaridad_i['Receta'] = recetas['Nombre receta']
+            similaridad_i = similaridad_i.rename(columns={0:'Similarity'})
+            similaridad_i = similaridad_i.sort_values(by=['Similarity'], ascending=False)
+
+            sug = similaridad_i['Receta'][0:5].values
+            msg = f"Te sugiero que preparemos {sug[0]} , ¿qué te parece?"
+            dispatcher.utter_message(text=msg)
+
+class ActionCooking(Action):
+
+    def name(self) -> Text:
+        return "action_cooking"
+
+
+class ActionSearchEntities(Action):
+
+    def name(self) -> Text:
+        return "action_search_entities"
+
+
+class ActionChangeRecipe(Action):
+
+    def name(self) -> Text:
+        return "action_change_recipe"
+
+
+class ActionIngredients(Action):
+
+    def name(self) -> Text:
+        return "action_ingredients"
